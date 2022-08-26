bd1dcf185445d830ecb721f013cf4101644325d5
Jasonchen-66 <jianbang@ualberta.ca>
2022-08-26 12:47:41 -0600

    personal post profile web project

---
+++ routes/auth-route.js
@@ -0,0 +1,78 @@
+const router = require("express").Router();
+const { Passport } = require("passport");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const User = require("../models/user-model");
+
+router.get("/login",(req,res)=>{
+    res.render("login",{user: req.user});
+});
+
+router.get("/signup",(req,res)=>{
+    res.render("signup",{user:req.user});
+});
+
+router.post("/login",passport.authenticate('local',{
+    failureRedirect: "/auth/login",
+    failureFlash:"Wrong email or password.",
+}),
+    (req,res)=>{
+        if(req.seesion.returnTo){
+            let newPath = req.session.returnTo;
+            req.session.returnTo = "";
+            res.redirect(newPath);
+        }else{
+            res.redirect("/profile");
+        }
+    }
+);
+
+router.post("/signup",async (req,res)=>{
+    let {name,email,password} = req.body;
+    //check if the data is already in db
+    const emailExist = await User.findOne({email});
+    if(emailExist){
+        req.flash("error_msg","Email has already been registered!");
+        res.redirect("/auth/signup");
+    }
+
+    const hash = await bcrypt.hash(password,10);
+    password = hash;
+    let newUser = new User({name,email,password});
+    try{
+        await newUser.save();
+        req.flash("success_msg","Registration succeeds. You can login now!");
+        res.redirect("/auth/login");
+    }catch (err){
+        req.flash("error_msg",err.errors.name.properties.message);
+        res.redirect("/auth/signup");
+    };
+});
+
+router.get("/logout",(req,res)=>{
+    req.logOut();
+    res.redirect("/");
+});
+
+router.get("/google",passport.authenticate(
+    "google",{
+    scope: ["profile","email"],
+    prompt: "select_account",
+})
+);
+
+router.get("/google",passport.authenticate("google",{
+    scope:["profile","email"],
+}));
+
+router.get("/google/redirect",passport.authenticate("google"),(req,res)=>{
+    if(req.seesion.returnTo){
+        let newPath = req.session.returnTo;
+        req.session.returnTo = "";
+        res.redirect(newPath);
+    }else{
+        res.redirect("/profile");
+    }
+});
+
+module.exports = router;
